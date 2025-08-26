34c24267f1fff3aa268bed23df4ee9068be5b746
James <ji.devenport123@gmail.com>
2025-08-26 14:31:30 +0100

    Retry logic implemented to allow paused db to warm up
    - Since I'm using a serverless solution to save money, the database takes time to "warm up" which means connection attempts can time out.
    - Simple retry logic solved those funny connection issues. Can't believe I didn't consider it earlier to be honest. Fixed and working now, in any case.
      Hooray!

--- app/services/subscriber_service.py
+++ app/services/subscriber_service.py
@@ -10,14 +10,23 @@ logger = logging.getLogger(__name__)
 
 def get_all_subscribers_by_postcodes(session: sessionmaker, postcodes: set[str]) -> list[Subscriber | None]:
     subscribers_with_postcodes: list[Subscriber] = []
-    with session() as session:
-        statement = (select(Subscriber, Postcode)
-                     .join(Subscriber.postcodes)
-                     .order_by(Subscriber.id, Postcode.id)
-                     .where(Postcode.postcode.in_(postcodes)))
-        for result in session.execute(statement):
-            subscribers_with_postcodes.append(result.Subscriber)
-    if len(subscribers_with_postcodes) == 0:
-        logger.warning(f"No subscribers with postcodes {postcodes} found in database.")
+    ATTEMPT_LIMIT = 5
+    attempt_number = 1
+    while attempt_number < ATTEMPT_LIMIT:
+        try:
+            with session() as session:
+                statement = (select(Subscriber, Postcode)
+                             .join(Subscriber.postcodes)
+                             .order_by(Subscriber.id, Postcode.id)
+                             .where(Postcode.postcode.in_(postcodes)))
+                for result in session.execute(statement):
+                    subscribers_with_postcodes.append(result.Subscriber)
+            if len(subscribers_with_postcodes) == 0:
+                logger.warning(f"No subscribers with postcodes {postcodes} found in database.")
+            return subscribers_with_postcodes
+        except Exception as e:
+            logger.error(f"Failed to get subscribers for postcodes {postcodes}: Retrying. "
+                         f"(Attempt {attempt_number} of {ATTEMPT_LIMIT}): {e}")
+            attempt_number += 1
+    logger.error("Attempt limit reached.")
     return subscribers_with_postcodes
-
